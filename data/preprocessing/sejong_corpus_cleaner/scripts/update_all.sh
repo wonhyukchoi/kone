e2074c59bfd466e81f56a0533b7ca35115474590
Wonhyuk Choi <woc997@g.harvard.edu>
2020-06-06 23:17:08 +0900

    remove lovit sejong corpus cleaner from repo

--- data/preprocessing/sejong_corpus_cleaner/scripts/update_all.sh
+++
@@ -1,6 +0,0 @@
-python build_corpus.py  --corpus_type type1
-python build_corpus.py  --corpus_type type2
-python build_corpus.py  --corpus_type type3
-python build_counter.py --corpus_type type1
-python build_counter.py --corpus_type type2
-python build_counter.py --corpus_type type3
